a8302893c15ad6a9846d4f2fe0b5b8c9dcd0a547
aleksei-demon <113286637+aleksei-demon@users.noreply.github.com>
2026-09-07 11:43:36 +0300

    Add files via upload

--- js/risunki.js
+++ js/risunki.js
@@ -208,111 +208,452 @@ window.CATALOGS.risunki = {
             name: "рисунок",
             img: "IMG_risunki/50.jpg",
         },
-
         {
-            name: "",
-            img: "IMG_risunki/.jpg",
+            name: "рисунок",
+            img: "IMG_risunki/51.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/52.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/53.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/54.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/55.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/56.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/57.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/58.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/59.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/60.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/61.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/62.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/63.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/64.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/65.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/66.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/67.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/68.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/69.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/70.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/71.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/72.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/73.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/74.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/75.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/76.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/77.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/78.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/79.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/80.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/81.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/82.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/83.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/84.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/85.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/86.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/87.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/88.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/89.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/90.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/91.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/92.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/93.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/94.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/95.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/96.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/97.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/98.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/99.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/100.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/101.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/102.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/103.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/104.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/105.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/106.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/107.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/108.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/109.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/110.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/111.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/112.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/113.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/114.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/115.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/116.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/117.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/118.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/119.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/120.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/121.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/122.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/123.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/124.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/125.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/126.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/127.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/128.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/129.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/130.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/131.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/132.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/133.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/134.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/135.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/136.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/137.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/138.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/139.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/140.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/141.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/142.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/143.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/144.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/145.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/146.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/147.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/148.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/149.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/150.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/151.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/152.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/153.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/154.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/155.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/156.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/157.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/158.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/159.jpg",
+        },
+        {
+            name: "рисунок",
+            img: "IMG_risunki/160.jpg",
         },
 
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
 
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
 
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
 
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
 
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
 
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
-
-        {
-            name: "",
-            img: "IMG_risunki/.jpg",
-        },
 
 
 
